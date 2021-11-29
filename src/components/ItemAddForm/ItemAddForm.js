e3bf033fce05ad95d6b7815e85e4b8a49c0769fc
Aleksandra <astarta1107@gmail.com>
2021-11-29 14:06:04 +0300

    added adding a new task

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -1,23 +1,38 @@
-import React from "react"
+import React, { Component } from "react"
 
 import "./ItemAddForm.css"
 
-const ItemAddForm = ({ onAddedItem }) => {
-  return (
-    <div className="item-add-form">
-      {/* <input
-        type="text"
-        className="form-control add-input"
-        placeholder="type to add"
-      ></input> */}
-      <button
-        className="btn btn-outline-secondary"
-        onClick={() => onAddedItem("New task added!")}
-      >
-        Add Item
-      </button>
-    </div>
-  )
-}
+export default class ItemAddForm extends Component {
+  state = {
+    label: "",
+  }
 
-export default ItemAddForm
+  onSubmit = event => {
+    event.preventDefault()
+    this.props.onAddedItem(this.state.label)
+    this.setState({
+      label: "",
+    })
+  }
+
+  onLabelAdd = event => {
+    this.setState({
+      label: event.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="What needs to be done"
+          onChange={this.onLabelAdd}
+          value={this.state.label}
+        />
+        <button className="btn btn-outline-secondary">Add Item</button>
+      </form>
+    )
+  }
+}
